17ebff810a1e9f9012c0bd3d1cd9eb0699966f1f
Lucas Alexander <lucas@alexanderdevelopment.net>
2016-02-09 11:50:02 -0600

    updated to read version number from assembly

--- AlexanderDevelopment.ConfigDataMover/About.cs
+++ AlexanderDevelopment.ConfigDataMover/About.cs
@@ -33,6 +33,20 @@ namespace AlexanderDevelopment.ConfigDataMover
         public About()
         {
             InitializeComponent();
+
+            string apacheText = @"Licensed under the Apache License, Version 2.0 (the 'License'); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.";
+
+            StringBuilder licenseSb = new StringBuilder();
+            licenseSb.AppendLine("Dynamics CRM Configuration Data Mover\n");
+            licenseSb.AppendLine("Version " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version + "\n");
+            licenseSb.AppendLine("Copyright " + DateTime.Now.Year.ToString() + " Lucas Alexander\n");
+            licenseSb.AppendLine(apacheText.Replace("'", "\""));
+            string aboutText = licenseSb.ToString();
+            this.label1.Text = aboutText;
         }
 
         private void button1_Click(object sender, EventArgs e)
